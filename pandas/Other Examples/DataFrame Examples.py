7f0efbe5879fb21a1a094664883f5529f0eeaa0b
LReyna <leo.reyna@gmail.com>
2023-09-09 16:07:06 -0400

    Adding DataFrames Examples

---
+++ pandas/Other Examples/DataFrame Examples.py
@@ -0,0 +1,21 @@
+# DataFrame Examples
+# LR 
+
+
+# empty dataframe
+import pandas as pd
+df = pd.DataFrame()
+print (df)
+
+# adding values
+import pandas as pd
+data = [1,2,3,4,5]
+df = pd.DataFrame(data)
+print (df)
+
+# adding values ex.2
+import pandas as pd
+data =['Alex', 28], ['Peter', 20], ['Joe', 23]
+df = pd.DataFrame(data)
+print (df)
+
