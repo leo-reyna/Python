66b6e18487e6edb8f768dcba4c381961bdef49f6
LReyna <leo.reyna@gmail.com>
2023-09-09 16:26:58 -0400

    Other Files

--- pandas/Other Examples/DataFrame Examples.py
+++ pandas/Other Examples/DataFrame Examples.py
@@ -19,3 +19,8 @@ data =['Alex', 28], ['Peter', 20], ['Joe', 23]
 df = pd.DataFrame(data)
 print (df)
 
+# Other Data Types List
+import pandas as pd
+records = [["John", 41,"EMP001"], ["Jane", 45, "EMP001"], ["Jean", 56],["Jake", 44,"EMP001"]]
+staff = pd.DataFrame(records, columns=['Name','Age', 'ID'])
+print(staff)
