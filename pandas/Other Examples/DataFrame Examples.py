de97b60aae137e1cafc3ea0663e8d0dcf9a8ecc0
LReyna <leo.reyna@gmail.com>
2023-09-10 16:08:28 -0400

    More Updates

--- pandas/Other Examples/DataFrame Examples.py
+++ pandas/Other Examples/DataFrame Examples.py
@@ -1,6 +1,7 @@
 # DataFrame Examples
 # LR 
-
+import  os
+os.system = ('cls')
 
 # empty dataframe
 import pandas as pd
@@ -24,3 +25,20 @@ import pandas as pd
 records = [["John", 41,"EMP001"], ["Jane", 45, "EMP001"], ["Jean", 56],["Jake", 44,"EMP001"]]
 staff = pd.DataFrame(records, columns=['Name','Age', 'ID'])
 print(staff)
+
+# Create a DataFrame from Dict of ndarrays / Lists. All the ndarrays must be of same length. If index is passed, then the length of the
+# index should equal to the length of the arrays. #If no index is passed, then by default, index will be range(n), where n is the array length.
+# running the code below,  note the values 0,1,2,3. They are the default index assigned to each using the function range(n).
+# with index 
+import pandas as pd
+data = {'name':['Joe', 'Paul', 'Thomas'], 'occupation': ['dentist','barber','farmer'], 'age' : [29, 30, 54]}
+data2 = {'name':['Jack', 'OJ', 'Jerome'], 'occupation': ['cleaner','actor','artist'], 'age' : [25, 36, 33],'location':['usa', 'europe', 'japan']}
+# with index: 
+# df = pd.DataFrame(data, index=['rnk1', 'rnk2', 'rnk3'])
+# without index:
+df1 = pd.DataFrame(data)
+df2 =pd.DataFrame(data2)
+
+# merging 2 dataframes
+df_merged = pd.concat([df1, df2], ignore_index=True, sort=False) 
+print (df_merged)
